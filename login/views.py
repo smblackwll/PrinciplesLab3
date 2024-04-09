5f00b95517cf5a3b54279d42c587a0aacd15448d
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-04-09 16:34:07 -0500

    Added the registration to views, and used the django html template for register.html.

--- login/views.py
+++ login/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')  # Redirect to login page
+    else:
+        form = UserCreationForm()
+    return render(request, 'register.html', {'form': form})
