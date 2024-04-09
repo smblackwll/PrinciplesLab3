18b150b162ad6b4d38b56d591b572bacdcd09187
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-04-09 16:04:26 -0500

    Added new app for authentication

--- PrinciplesLab3/settings.py
+++ PrinciplesLab3/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    "auth.apps.AuthConfig"
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,8 @@ ROOT_URLCONF = 'PrinciplesLab3.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates']
+        ,
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
