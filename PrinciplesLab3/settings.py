774bbba62286f2ae1d914b3ee6e153d1e2128bd8
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-04-09 16:13:52 -0500

    Removed old login app, replaced with login app.

--- PrinciplesLab3/settings.py
+++ PrinciplesLab3/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "auth.apps.AuthConfig"
+    "login.apps.LoginConfig"
 ]
 
 MIDDLEWARE = [
@@ -101,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'auth.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
