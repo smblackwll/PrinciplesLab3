23986ecf03dd0f7ab6b1c6c79975d28d81cacc29
Spencer Blackwell <spencerblackwell02@gmail.com>
2024-04-09 16:25:56 -0500

    Made the site default to the home page

--- PrinciplesLab3/urls.py
+++ PrinciplesLab3/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home.views import home
+
+import home
 
 urlpatterns = [
+    path('', home.views.home),
     path('admin/', admin.site.urls),
 ]
