e293de4e4ca5adb6c07970d520ce72c7314ce2cd
SnFoil <samwitte@me.com>
2024-04-25 18:23:45 -0500

    Fixed issues with main

--- home/models.py
+++ home/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+from login.models import User
+
+
 # Create your models here.
+
+class Comment(models.Model):
+    related_page = models.CharField(max_length=20)
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE
+    )
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
